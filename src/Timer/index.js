831d3556b7b73168ff0b2ae186c55a87a9e5c505
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-03 12:21:26 +0530

    timer component functionality

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Timer( props ) {
-	return (
-		<div className="timer">Time Remaining: 10</div>
-	)
+
+
+class Timer extends Component {
+
+	state = {
+		timeRemaining: 10
+	};
+
+	startTimer = () => {
+		const interval = setInterval( () => {
+			if ( this.state.timeRemaining === 0 ) {
+				clearInterval( interval );
+				return;
+			}
+			this.setState( {
+				timeRemaining: this.state.timeRemaining - 1
+			} )
+		}, 1000 );
+	};
+
+	render() {
+		return ( <div>
+			<div className="timer">{ `Time Remaining: ${this.state.timeRemaining}` }</div>
+		</div>
+		);
+	};
+
 }
 
 export default Timer;
