7f4ba451918d842da629a5377770d2e4ebda0a27
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-03 12:45:02 +0530

    dynamic number board component

--- src/GameBoard/NumberBoard/index.js
+++ src/GameBoard/NumberBoard/index.js
@@ -1,17 +1,10 @@
 import React from "react";
+import Utils from "../../Utils";
 
 function NumberBoard( props ) {
 	return (
 		<div className="right">
-			<button className="number">1</button>
-			<button className="number">2</button>
-			<button className="number">3</button>
-			<button className="number">4</button>
-			<button className="number">5</button>
-			<button className="number">6</button>
-			<button className="number">7</button>
-			<button className="number">8</button>
-			<button className="number">9</button>
+			{ Utils.range( 1, 9 ).map( number => <button key={ number } className="number">{ number }</button> ) }
 		</div>
 	);
 }
