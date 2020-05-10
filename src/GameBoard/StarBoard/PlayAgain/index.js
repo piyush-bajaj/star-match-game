aaa92fae3f6e24126dfac3b2161e6e7daad507b4
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-05-10 19:15:30 +0530

    CSS improvements for play again message

--- src/GameBoard/StarBoard/PlayAgain/index.js
+++ src/GameBoard/StarBoard/PlayAgain/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 function PlayAgain( props ) {
 
 	const gameStatusDisplay = () => {
-		return props.gameStatus === 'won' ? <div><div>Viola, You have won the game.</div><div>!!!Congratulations!!!</div></div> : <div><div>Sorry, you lost the game.</div><div>!!!Better Luck Next Time!!!</div></div>;
+		return props.gameStatus === 'won' ?
+			<div className="message" style={ { color: 'green' } }><div>Viola, You have won the game.</div><div>!!!Congratulations!!!</div></div> :
+			<div className="message" style={ { color: 'red' } }><div>Sorry, you lost the game.</div><div>!!!Better Luck Next Time!!!</div></div>;
 	}
 
 	return (
